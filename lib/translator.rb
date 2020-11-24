b87a38a10bde60e0677cc6473ea328d6581b4333
Abbie Coghlan <abbie.coghlan@gmail.com>
2020-11-24 16:29:47 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,12 +7,9 @@ def load_library(file)
   emoticon_library = YAML.load_file(file)
   
   emoticon_library.collect do |key, value|
-  emoticon_library[key] = {:english => value[0], :japanese => value[1]}  
+    emoticon_library[key] = {:english => value[0], :japanese => value[1]}  
   end 
-  
-#  emoticon_library.each do |key, value
-#  end
- emoticon_library
+  emoticon_library
 end
 
 def get_japanese_emoticon
